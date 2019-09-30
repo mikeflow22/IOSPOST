973e29147db7021804973e71491cb532c936b888
Michael R.F <mrflowers22@uwalumni.com>
2019-09-30 14:50:42 -0600

    done with part one

--- IOS-Post/View Controllers/PostListViewController.swift
+++ IOS-Post/View Controllers/PostListViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class PostListViewController: UIViewController {
 
-    var postController: PostController()
+    var refreshControl = UIRefreshControl()
+    
+    private lazy var formmatter: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
+    }()
+    var postController = PostController()
     
     @IBOutlet weak var myTableView: UITableView!
     
@@ -18,10 +25,50 @@ class PostListViewController: UIViewController {
         super.viewDidLoad()
         myTableView.delegate = self
         myTableView.dataSource = self
+        myTableView.estimatedRowHeight = 45
+        myTableView.rowHeight = UITableView.automaticDimension
+        myTableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(refreshControlPulled), for: .valueChanged)
+        postController.fetchPosts {
+            print("Called fetch posts in the view did load")
+            DispatchQueue.main.async {
+                self.reloadTableView()
+            }
+        }
+    }
+    
+    @objc func refreshControlPulled(){
+        postController.fetchPosts {
+            print("Called the fetdh post function inside of the refresh control")
+            DispatchQueue.main.async {
+                self.refreshControl.endRefreshing()
+                self.reloadTableView()
+            }
+        }
+    }
+    
+    func reloadTableView(){
+//            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+            self.myTableView.reloadData()
+//            UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
 
 }
 
 extension PostListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postController.posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
+        let post = postController.posts[indexPath.row]
+        cell.textLabel?.text =  post.text
+        let date = Date(timeIntervalSince1970: post.timestamp)
+        cell.detailTextLabel?.text = "\(post.username) on \(formmatter.string(from: date))"
+        return cell
+    }
+    
+    
     
 }
